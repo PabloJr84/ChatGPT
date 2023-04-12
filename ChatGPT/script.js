59a0f4ee58e44d042b9241e447733fce2f3325ad
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-04-12 01:13:13 -0300

    Atualização final do chatGPT

--- ChatGPT/script.js
+++ ChatGPT/script.js
@@ -1,62 +1,55 @@
-const imputQuestion = document.getElementById
-("inputQuestion");
+const inputQuestion = document.getElementById("inputQuestion");
 const result = document.getElementById("result");
 
-inputQuestion.addEventListener("keypress", (e) =>{
-    if (inputQuestion.value && e.key === "Enter")
-    SendQuestion();
+inputQuestion.addEventListener("keypress", (e) => {
+  if (inputQuestion.value && e.key === "Enter") SendQuestion();
 });
 
-const OPENAI_API_KEY =
-"";
+const OPENAI_API_KEY = "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";
 
-function SendQuestion(){
-    var sQuestion = inputQuestion.value;
+function SendQuestion() {
+  var sQuestion = inputQuestion.value;
 
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + OPENAI_API_KEY,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: sQuestion,
+      max_tokens: 2048, // tamanho da resposta
+      temperature: 0.5, // criatividade na resposta
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (result.value) result.value += "\n";
 
-    fetch("https://api.openai.com/v1/completions",{
-        method: "POST",
-        headers: {
-            Accept:"application/json",
-        "Content-Type": "application/json",
-        Authorization: "Bearer" + OPENAI_API_KEY,
-        },
-        body: JSON.stringify({
-            model: "text-davinci-003",
-            prompt: sQuestion,
-            max_tokens: 2048, //tamanho da resposta
-            temperature: 0.5, // criatividade na resposta
-        }),
+      if (json.error?.message) {
+        result.value += `Error: ${json.error.message}`;
+      } else if (json.choices?.[0].text) {
+        var text = json.choices[0].text || "Sem resposta";
+
+        result.value += "Chat GPT: " + text;
+      }
+
+      result.scrollTop = result.scrollHeight;
     })
-    .then((response)=> response.json())
-    .then ((json) =>{
-        if (result.value) result.value += "\n";
-
-        if(json.error?.message){
-            result.value += `Error: ${json.error.message}
-            `;
-
-        } else if (json.choice?.[0].text){
-            var text =json.choice[0].text || "Sem resposta";
-
-            result.value += "Chat GPT" + text;
-        }
-        result.scrollTop = result.scrollHeight;
-    })
-    .catch((error)=>console.error("Error",error))
-    .finally(()=> {
-        inputQuestion.value = "";
-        inputQuestion.disabled = false;
-        inputQuestion.focus();
-
+    .catch((error) => console.error("Error:", error))
+    .finally(() => {
+      inputQuestion.value = "";
+      inputQuestion.disabled = false;
+      inputQuestion.focus();
     });
 
-    if (result.value) result.value += "\n\n\n";
+  if (result.value) result.value += "\n\n\n";
 
-    result.value += ` Eu: ${sQuestion}`;
-    inputQuestion.value = "Carregando...";
-    inputQuestion.disabled = true;
+  result.value += `Eu: ${sQuestion}`;
+  inputQuestion.value = "Carregando...";
+  inputQuestion.disabled = true;
 
-    result.scrollTop = result.scrollHeight;
-
+  result.scrollTop = result.scrollHeight;
 }
